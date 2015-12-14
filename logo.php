4546a7ee1b19c4a1e0ed4d595b0e1b7861a1d897
Dominik García <dominikgbertapelle@lifevert.com>
2015-12-14 10:57:11 -0600

    fixed styling

--- logo.php
+++ logo.php
@@ -1,1 +1,1 @@
-<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/src/media/LogoMAQROfondogris.jpg" />
+<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/src/media/LogoMAQRO.png" />
